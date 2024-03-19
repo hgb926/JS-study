bf74385b1e72cb3714b06b25693d84331719eab5
hgb926 <hgb926@naver.com>
2024-03-20 08:57:56 +0900

    # 0319

---
+++ 1. core javascript/1. 자바스크립트 기초/1. 변수.js
@@ -0,0 +1,31 @@
+
+var result;
+
+result = 10 + 20; // 값을 변수에 할당(대입)
+
+var multiple = result * 3; // 90
+
+console.log(multiple);
+
+var fruit;
+fruit = '자몽';
+console.log(fruit);
+
+var food = '볶음밥';
+food = '돈가스' // 재할당
+console.log(food);
+
+// 변수 이름 규칙
+// var 7number; (x)
+
+// var user name; (x)
+
+//var current_login_user_phone_number;  (파이썬에서 많이 사용)
+
+//var currentLoginUserPhoneNumber;  camel case
+
+var apple;
+var Apple;
+var APPLE;
+
+
---
+++ 1. core javascript/1. 자바스크립트 기초/4. 비교연산자.js
@@ -0,0 +1,32 @@
+
+var a = 5;
+var b = '5';
+
+console.log(a === b);
+
+console.log('================');
+
+console.log('0' == '');
+console.log(0 == '');
+console.log('0' == 0);
+console.log(false == 'false');
+console.log(false == 0);
+
+// 대소 비교
+console.log('================');
+
+console.log('가' < 'b');
+console.log('☆' < '하');
+          // 97    65
+console.log('a' < 'A');
+
+
+
+
+
+
+
+
+
+
+
